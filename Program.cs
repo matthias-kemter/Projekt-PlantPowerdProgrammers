e9c4a543bc44fa263c1a2895deacf231dcd0730f
Enrique Berrios <enrique.berrios@student.tu-freiberg.de>
2022-07-13 09:33:03 +0200

    Klasse Bäume angelegt, mit CSV Einlesen (exceptions müssen noch gefixt werden)

--- Program.cs
+++ Program.cs
@@ -1,1 +1,73 @@
-﻿
+﻿using System;
+using System.Collections;
+
+
+namespace Baumprojekt
+{
+    class Baeume 
+    {
+        public float X_Koordinate {get; set;}
+        public float Y_Koordinate {get; set;}
+        public int ObjektID {get; set;}
+        public int ID {get; set;}
+        public int Objektschluessel {get; set;}
+        public int Baumnummer {get; set;}
+        public string ?Baumart {get; set;}
+        public int Pflanzdatum {get; set;}
+
+        public Baeume() { }
+        public Baeume(string csvString)
+        {
+            string [] csvEntries = csvString.Split(',');
+            
+            // LEFT TO CHECK: Convert ToInt16/32/64
+            X_Koordinate = float.Parse(csvEntries[0]);
+            Y_Koordinate = float.Parse(csvEntries[1]);
+            ObjektID = Convert.ToInt16(csvEntries[2]);
+            ID = Convert.ToInt16(csvEntries[3]);
+            Objektschluessel = Convert.ToInt16(csvEntries[4]);
+            Baumnummer = Convert.ToInt16(csvEntries[5]);
+            Baumart = csvEntries[6];
+            Pflanzdatum = Convert.ToInt16(csvEntries[7]);
+        }
+
+        public override string ToString()
+        {
+            return  "X-Koordinate: " + X_Koordinate + 
+                    "\nY-Koordinate: " + Y_Koordinate + 
+                    "\nObjektID: " + ObjektID +
+                    "\nID: " + ID +
+                    "\nObjektschlüssel: " + Objektschluessel +
+                    "\nBaumnummer: " + Baumnummer +
+                    "\nBaumart: " + Baumart + 
+                    "\nPflanzdatum: " + Pflanzdatum;
+        }
+    }
+    class Program
+    {    static void Main(string[] args)
+        {
+            // Dateiname von csv
+            string pathBaeumeCsv = @"Neu.csv";
+
+            // Liste von Bäumen erstellen
+            List<Baeume> BaumListe = new List<Baeume>();
+
+            // Liste von Bäumen mit csv Einträgen füllen
+            if(File.Exists(pathBaeumeCsv))
+            {
+                // Zeile lesen
+                string[] baeumeAsCsvString = File.ReadAllLines(pathBaeumeCsv);
+
+                // Daten in Liste schreiben
+                BaumListe.Add(new Baeume(baeumeAsCsvString[0]));
+
+                // Daten ausgeben lassen
+                foreach (Baeume aBaum in BaumListe)
+                {
+                    System.Console.WriteLine(aBaum.ToString());
+                    System.Console.WriteLine("Hello World!");
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,20 +3,18 @@ _Projektarbeit vom Team PlantPowerdProgrammers_
 
 **Ziele:** 
 
-1)
--   Sortierung und Tabellarische Auflistung aller Baumgattungen/-arten (lat. Name)
+1) **Gattungen**
+-   Sortierung und Tabellarische Auflistung aller       Baumgattungen/-arten (lat. Name)
 -   Zählung der Gattungen/ Arten  
 -   Darstellung der Gattungen auf je einer Karte
-2)    
+
+2) **Alter**   
 -   Sortierung nach Pflanzdatum
 -   Darstellung der 50 ältesten Bäume auf einer Karte
 -   Darstellung der Pflanzdaten in einem Diagramm (Anzahl/Jahr)
+-   Bereich der ältesten 50 Bäume ermitteln (Mittelpunkt/ Umkreis)
 
-3)
--   _(wird später bearbeitet bzw. verändert wenn Ergebnisse der vorherigen Aufgaben vorliegen)_ 
- Bereich der ältesten 50 Bäume ermitteln (Mittelpunkt/ Umkreis)
-
-
+**UML-Diagramm:**
 
 **Dateien/ Daten:**
 - Geschütze Bäume im Großraum Chemnitz: _https://www.govdata.de/web/guest/suchen/-/details/baeume_
